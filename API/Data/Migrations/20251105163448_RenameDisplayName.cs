3801dac1478e979002dac58157d43e5d317752df
hhoang82 <celcron@yahoo.com>
2025-11-05 13:23:34 -0800

    End of section 2

---
+++ API/Data/Migrations/20251105163448_RenameDisplayName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDisplayName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplyName",
+                table: "Users",
+                newName: "DisplayName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "Users",
+                newName: "DisplyName");
+        }
+    }
+}
