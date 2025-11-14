5e24fda4b71461a13782c31f277b5dd6d9e5a3de
hhoang82 <celcron@yahoo.com>
2025-11-13 22:03:22 -0800

    End of section 14

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -10,10 +10,23 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
 
   const generateCacheKey = (url: string, params: HttpParams): string => {
     const paramString = params.keys().map(key => `${key}=${params.get(key)}`).join('&');
-    return paramString ? `${url}${paramString}` : url;
+    return paramString ? `${url}?${paramString}` : url;
+  }
+
+  const invalidateCache = (urlPattern: string) => {
+    for (const key of cache.keys()) {
+      if (key.includes(urlPattern)) {
+        cache.delete(key);
+        console.log(`Cache invalidated for: ${key}`);
+      }
+    }
   }
 
   const cacheKey = generateCacheKey(req.url, req.params);
+
+  if (req.method.includes('POST') && req.url.includes('/likes')) {
+    invalidateCache('/likes')
+  }
 
   if (req.method === 'GET') {
     const cachedResponse = cache.get(cacheKey);
--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,18 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { of } from 'rxjs';
+import { LikeService } from './like-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
   private accountService = inject(AccountService);
+  private likesService = inject(LikeService);
 
   init() {
     const userString = localStorage.getItem('user');
     if (!userString) return of(null);
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
+    this.likesService.getLikeIds();
 
     return of(null)
   }
